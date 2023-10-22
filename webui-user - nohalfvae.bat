3593b09bc1b5b499077b33ec561a3a2c70748184
asCeo <axilesoft@users.noreply.github.com>
2023-10-22 15:40:45 +0800

    Drop Commit

---
+++ webui-user - nohalfvae.bat
@@ -0,0 +1,7 @@
+@echo off
+set PYTHON=
+set GIT=
+set VENV_DIR=
+set COMMANDLINE_ARGS=--xformers  --no-half-vae 
+rem  --api --ckpt-dir "Q:\DL1\SdTemp"  --ckpt-dir "C:\ai\Sdmodels"   
+call webui.bat
